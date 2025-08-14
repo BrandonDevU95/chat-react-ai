682419a01a7a997597923beff2040a64165b76a8
BrandonDevU95 <brandon.vargas1095@hotmail.com>
2025-08-14 03:57:30 -0600

    feat: implement authentication check and loading state in AdminLayout

--- src/layouts/admin.layout.tsx
+++ src/layouts/admin.layout.tsx
@@ -1,6 +1,26 @@
-import { Outlet } from 'react-router';
+import { Navigate, Outlet } from 'react-router';
+
+import { useSigninCheck } from 'reactfire';
 
 const AdminLayout = () => {
+	const { status, data: signInCheckResult, hasEmitted } = useSigninCheck();
+
+	console.log({
+		status,
+		signInCheckResult,
+		hasEmitted,
+	});
+
+	//Mostrar Loading mienstras se verifica el estado de autenticación
+	if (status === 'loading' || !hasEmitted) {
+		return <div>Loading...</div>;
+	}
+
+	//Redirigir si el usuario no esta autenticado
+	if (status === 'success' && signInCheckResult?.signedIn === false) {
+		return <Navigate to="/auth/login" replace />;
+	}
+
 	return (
 		<div>
 			<Outlet />
